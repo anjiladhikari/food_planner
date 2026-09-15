85c0d6cfcd0ccb8a09803966c41f5c29d9defc94
anjiladhikari <anjiladlee10@gmail.com>
2026-09-15 17:09:23 +1000

    done

--- src/components/RagChat.jsx
+++ src/components/RagChat.jsx
@@ -26,7 +26,7 @@ export default function RagChat() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/chat", {
+      const response = await fetch("https://food-planner-iba3.onrender.com/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
